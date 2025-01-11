882ea5f806e07fadf617c1b686a9dc0fd0952f5c
Alvarorc99 <alvaro.rodriguez.casillas@gmail.com>
2025-01-11 14:26:49 +0100

    Hecho el web scraping con enlace e imagen de las palas

--- web_scraping/padel_nuestro_scraping.py
+++ web_scraping/padel_nuestro_scraping.py
@@ -1,16 +1,34 @@
 import os
+import re
 import time
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-#imagen_dir = "C:/Users/alvar/TFG/chatbot_aws/dataset_padel_nuestro/images"
 
-# Lista para almacenar los enlaces
+# Inicializar las listas con un valor por defecto
+marca = []
+color = []
+balance = []
+nucleo = []
+cara = []
+formato = []
+nivel_juego = []
+acabado = []
+forma = []
+dureza = []
+superficie = []
+tipo_juego = []
+jugador = []
+coleccion_jugador = []
+nombre_pala = []
+precio = []
+descripcion = []
 enlaces = []
+imagen = []
 
 # Número total de páginas a procesar
-total_paginas = 41
+total_paginas = 40
 
 # Iterar a través de cada página
 for page_number in range(1, total_paginas + 1):
@@ -36,24 +54,6 @@ for page_number in range(1, total_paginas + 1):
 
 print(f"Se encontraron {len(enlaces)} enlaces de palas.")
 
-# Inicializar las listas con un valor por defecto
-marca = []
-color = []
-balance = []
-nucleo = []
-cara = []
-formato = []
-nivel_juego = []
-acabado = []
-forma = []
-dureza = []
-superficie = []
-tipo_juego = []
-jugador = []
-coleccion_jugador = []
-nombre_pala = []
-precio = []
-descripcion = []
 
 # Función para extraer características de cada pala
 def obtener_caracteristicas_pala(enlace):
@@ -74,7 +74,7 @@ def obtener_caracteristicas_pala(enlace):
     current_marca = current_color = current_balance = current_nucleo = current_caras = current_formato = ""
     current_nivel_juego = current_acabado = current_forma = current_superficie = current_dureza = ""
     current_tipo_juego = current_jugador = current_coleccion_jugador = ""
-    current_descripcion = current_nombre = current_precio = ""
+    current_descripcion = current_nombre = current_precio = current_imagen_url =""
 
     # Asegurarse de que el contenedor fue encontrado
     if container:
@@ -134,29 +134,12 @@ def obtener_caracteristicas_pala(enlace):
     if precio_tag:
         current_precio = precio_tag.text.strip()
     
-    # Extraer la imagen desde el contenedor con la clase 'fotorama__stage__frame'
-    # Buscar la imagen directamente con la clase adecuada
-    # imagen_tag = soup.find('img', class_='fotorama__img magnify-opaque')
-
-    # current_imagen_url = ""
-    # if imagen_tag and 'src' in imagen_tag.attrs:
-    #     current_imagen_url = imagen_tag['src']  # Obtener la URL de la imagen desde el atributo 'src'
-
-    # # Descargar y guardar la imagen si la URL es válida
-    # if current_imagen_url:
-    #     image_filename = os.path.join(imagen_dir, os.path.basename(current_imagen_url))
-    #     try:
-    #         response = requests.get(current_imagen_url, stream=True)
-    #         if response.status_code == 200:
-    #             with open(image_filename, 'wb') as f:
-    #                 f.write(response.content)
-    #             print(f"Imagen descargada con éxito: {image_filename}")
-    #         else:
-    #             print(f"Error al descargar la imagen: {current_imagen_url}")
-    #     except requests.RequestException as e:
-    #         print(f"Error al intentar descargar la imagen: {str(e)}")
-
-
+    # Imagen de la pala (extraído desde el <img> con clase 'product-image')
+    match = re.search(r'https://www\.padelnuestro\.com/media/catalog/product/[\w\-\_/.]+', page.text)
+    if match:
+        current_imagen_url = match.group(0)  # Usar group(0) para la URL completa
+    
+        
     # Añadir los valores de cada pala a las listas
     marca.append(current_marca)
     color.append(current_color)
@@ -175,7 +158,7 @@ def obtener_caracteristicas_pala(enlace):
     nombre_pala.append(current_nombre)
     precio.append(current_precio)
     dureza.append(current_dureza)
-    #imagen.append(current_imagen)
+    imagen.append(current_imagen_url)
 
     time.sleep(1)  # Esperar 1 segundo entre solicitudes para evitar sobrecargar el servidor
     print(f"Pala {nombre_pala} procesada con éxito.\n")
@@ -202,12 +185,13 @@ df = pd.DataFrame({
     'Tipo de juego': tipo_juego,
     'Nivel de Juego': nivel_juego,
     'Colección Jugadores': coleccion_jugador,
-    'Descripción': descripcion,
     'Enlace': enlaces,
+    'Imagen': imagen,
+    'Descripción': descripcion,
 })
 
 # Guardar el DataFrame en un archivo Excel
-df.to_excel('C:/Users/alvar/TFG/chatbot_aws/dataset_padel_nuestro/palas_padelnuestro_actualizado.xlsx', index=False)
+df.to_excel('C:/Users/alvar/TFG/PADELMASTER BACKEND/dataset_padel_nuestro/palas_padelnuestro_actualizado.xlsx', index=False)
 
 # Mostrar el DataFrame final
 print("Datos de las palas guardados exitosamente en 'palas_padelnuestro_actualizado.xlsx'.")
